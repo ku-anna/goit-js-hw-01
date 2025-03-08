da665b29adc0f9bf543cec6b565de4c8f1e5c852
Ann K <kubareva.ann@gmail.com>
2025-03-08 16:23:42 +0200

    js hw 1 fixed after revision

--- js/task-2.js
+++ js/task-2.js
@@ -1,4 +1,8 @@
 function getShippingMessage(country, price, deliveryFee) {
   const shippingPrice = price + deliveryFee;
-  return `Shipping to ${country} will const ${totalPrice} credits`;
+  return `Shipping to ${country} will cost ${shippingPrice} credits`;
 }
+
+console.log(getShippingMessage('Australia', 120, 50));
+console.log(getShippingMessage('Germany', 80, 20));
+console.log(getShippingMessage('Sweden', 100, 20));
--- js/task-3.js
+++ js/task-3.js
@@ -4,3 +4,6 @@ function getElementWidth(content, padding, border) {
   const borderWidth = parseFloat(border) * 2;
   return contentWidth + paddingWidth + borderWidth;
 }
+console.log(getElementWidth('50px', '8px', '4px'));
+console.log(getElementWidth('60px', '12px', '8.5px'));
+console.log(getElementWidth('200px', '0px', '0px'));
