c373772708bf30144e4f6a0b6b87d82832fe44a8
Ann K <kubareva.ann@gmail.com>
2025-03-08 11:21:48 +0200

    hw ready

--- js/task-2.js
+++ js/task-2.js
@@ -1,9 +1,4 @@
 function getShippingMessage(country, price, deliveryFee) {
-    const shippingPrice = price + deliveryFee;
-    return `Shipping to ${country} will const ${totalPrice} credits`;
+  const shippingPrice = price + deliveryFee;
+  return `Shipping to ${country} will const ${totalPrice} credits`;
 }
-
-
-console.log(getShippingMessage("Australia", 120, 50)); // "Shipping to Australia will cost 170 credits"
-console.log(getShippingMessage("Germany", 80, 20)); // "Shipping to Germany will cost 100 credits"
-console.log(getShippingMessage("Sweden", 100, 20)); // "Shipping to Sweden will cost 120 credits"
